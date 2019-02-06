c4250a9d2a4d5d8392bc7fa540329df8680712df
Amanda Ekaratih Siswojo <aes793@nyu.edu>
2019-02-06 10:22:16 -0500

    Week 2 - Camera Controller

---
+++ TAS_W02_Assignment/Week 2 - Camera Controller/Assets/LookAtButtonForLAUB.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LookAtUnityBezierScript))]
+public class LookAtButtonForLAUB : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        LookAtUnityBezierScript _myLAUB = (LookAtUnityBezierScript)target; //target is variable keyword for editor looks for obj to cast class 
+
+        DrawDefaultInspector();
+
+
+        if (GUILayout.Button("Test Button"))
+        {
+            Debug.Log("Button pressed!");
+
+            BezierExample newBe = _myLAUB.myModel.gameObject.AddComponent<BezierExample>();
+
+            if(_myLAUB.curveList.Count > 0)
+            {
+                BezierExample lastBe = _myLAUB.curveList[_myLAUB.curveList.Count - 1];
+                newBe.startPoint = lastBe.endPoint;
+                newBe.endPoint = lastBe.endPoint;
+                newBe.endTangent = lastBe.endTangent;
+                //if you are creating a subsequent bezier
+            }
+           
+
+            
+
+            _myLAUB.curveList.Add(newBe);
+
+            
+        }
+           
+       
+    }
+    
+}
---
+++ TAS_W02_Assignment/Week 2 - Camera Controller/Assets/LookAtUnityBezierScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; //modify things that are specific to tools like buttons
+
+public class LookAtUnityBezierScript : MonoBehaviour {
+    //[ExecuteInEditMode] //only use in debug scripts. run script when you are not in play mode.
+
+    public GameObject marker;
+    public BezierExample bezEx;
+    public Transform myModel;
+
+    public List<BezierExample> curveList = new List<BezierExample>();
+
+	// Use this for initialization
+	public void Start () {
+        //Run through 100 points and place a marker at those points on the bezier curver.
+
+        /*
+         * Step 1 : For loop throught 100 points between 0 & 1
+         * Step 2 : Pass that fraction to a curve calc to find the resultant V3
+         * Step 3 : Place a Marker at that V3
+         * 
+         */
+
+        PutPointsOnCurve();
+             
+	}
+
+    private void PutPointsOnCurve()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            float t = (float)i / 100;
+            Vector3 pointOncurve = CalculateBezier(bezEx, t);
+            Instantiate(marker, pointOncurve, Quaternion.identity, null);
+            CalculateBezier(bezEx, t);
+        }
+    }
+	
+    Vector3 CalculateBezier(BezierExample curveData, float t)
+    {
+        Vector3 a = curveData.startPoint;
+        Vector3 b = curveData.startTangent;
+        Vector3 c = curveData.endTangent;
+        Vector3 d = curveData.endPoint;
+
+        Vector3 ab = Vector3.Lerp(a, b, t);
+        Vector3 bc = Vector3.Lerp(b, c, t);
+        Vector3 cd = Vector3.Lerp(c, d, t);
+
+        Vector3 abc = Vector3.Lerp(ab, bc, t);
+        Vector3 bcd = Vector3.Lerp(bc, cd, t);
+
+        Vector3 final = Vector3.Lerp(abc, bcd, t);
+
+        return final;
+
+    }
+
+	
+}
